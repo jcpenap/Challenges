dd9c4681e093df334e0127bcafdd249f6da34878
Juan Carlos Pena Pena <juancarlos.pena@globant.com>
2022-11-15 22:20:19 -0500

    first commit

---
+++ authentication-service/src/test/kotlin/com/devchallenges/authenticationservice/AuthenticationServiceApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.devchallenges.authenticationservice
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class AuthenticationServiceApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
